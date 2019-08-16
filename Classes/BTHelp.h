0b2369df3b191187e4ae601db7b8e38af372a10e
stonemover <stonemover@163.com>
2019-08-16 15:37:07 +0800

    1.新版本保存

--- Classes/BTHelp.h
+++ Classes/BTHelp.h
@@ -6,13 +6,9 @@
 //  Copyright © 2019 stonemover. All rights reserved.
 //
 #import "BTUtils.h"
-#import "BTButton.h"
 #import "BTPermission.h"
 #import "BTKeyboardHelp.h"
-#import "BTLineView.h"
 #import "BTIconHelp.h"
 #import "BTLocation.h"
 #import "BTTimerHelp.h"
-#import "UIView+BTViewTool.h"
-#import "UIImage+BTImage.h"
 #import "BTModel.h"
