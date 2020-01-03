fbaecb9e81871ef853d75c9fdbdb878e9054bc16
stonemover <stonemover@163.com>
2020-01-03 11:38:14 +0800

    1.修改月份

--- Classes/NSDate+BTDate.m
+++ Classes/NSDate+BTDate.m
@@ -92,7 +92,7 @@
 
 + (NSDate*)dateYMD:(NSString*)dateStr{
     NSDateFormatter * formatter =[[NSDateFormatter alloc] init];
-    formatter.dateFormat=@"YYYY-MM-dd";
+    formatter.dateFormat=@"yyyy-MM-dd";
     return [formatter dateFromString:dateStr];
 }
 
@@ -100,14 +100,14 @@
 
 + (NSDate*)dateYMDHMS:(NSString*)dateStr{
     NSDateFormatter * formatter =[[NSDateFormatter alloc] init];
-    formatter.dateFormat=@"YYYY-MM-dd HH:mm:ss";
+    formatter.dateFormat=@"yyyy-MM-dd HH:mm:ss";
     return [formatter dateFromString:dateStr];
 }
 
 
 + (NSDate*)dateYMDHM:(NSString*)dateStr{
     NSDateFormatter * formatter =[[NSDateFormatter alloc] init];
-    formatter.dateFormat=@"YYYY-MM-dd HH:mm";
+    formatter.dateFormat=@"yyyy-MM-dd HH:mm";
     return [formatter dateFromString:dateStr];
 }
 
