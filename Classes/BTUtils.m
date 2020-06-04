4bd4a8aedc888810697714bdf63190218fa4a04a
stonemover <stonemover@163.com>
2020-06-04 15:44:07 +0800

    1.更新

--- Classes/BTUtils.m
+++ Classes/BTUtils.m
@@ -703,7 +703,7 @@
 + (NSString*)MD5:(NSString*)str{
     const char *cStr = [str UTF8String];
     unsigned char digest[CC_MD5_DIGEST_LENGTH];
-    CC_MD5( cStr, strlen(cStr), digest ); // This is the md5 call
+    CC_MD5(cStr, (CC_LONG)strlen(cStr), digest);
     NSMutableString *output = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH * 2];
     for(int i = 0; i < CC_MD5_DIGEST_LENGTH; i++){
         [output appendFormat:@"%02x", digest[i]];
